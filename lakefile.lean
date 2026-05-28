322433d381070b90f2cbaacb62df09a5fa62bca8
jackyfan01 <jackyfan01@users.noreply.github.com>
2026-05-28 22:58:24 +0800

    Upgrade to Mathlib4 v4.30.0, fix CI workflow (add lake update)

--- lakefile.lean
+++ lakefile.lean
@@ -11,4 +11,4 @@ lean_lib HuangzhongLaw72 where
   srcDir := "."
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4" @ "v4.8.0"
+  "https://github.com/leanprover-community/mathlib4" @ "v4.30.0"
